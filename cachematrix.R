69f636fbfd4f80e06bf49b223bb347d5d73eac2c
Pratik Gehlot <pratik.aimachine@gmail.com>
2020-10-11 17:47:46 +0530

    Assignment was completed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## We are trying to build a functions that are able to cache the values, because
+## during smaller computations you wont see improvement in speed, but as the number
+## of computations increases you need to cache the values for faster excecution
 
-## Write a short comment describing this function
+## The function is trying to maintain cache for our use
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() {x}
+  setInverse <- function(inverse){
+    inv <<- inverse
+  }
+  getInverse <- function(){inv}
+  list(set = set , get = get , setInverse = setInverse, getInverse = getInverse)
+  
 }
 
 
-## Write a short comment describing this function
+## The function is trying to make cache for our use
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return (inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat , ...)
+  x$setInverse(inv)
+  inv
+  
 }
